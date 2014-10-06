c3f745861979fbbc2d64f129eaa3d9c498d7c5af
Stanislav Artemkin <artemkin@gmail.com>
2014-10-06 19:29:02 +0400

    Initial Commit

---
+++ Parsee/Parsee.h
@@ -0,0 +1,19 @@
+//
+//  Parsee.h
+//  Parsee
+//
+//  Created by Stas on 06.10.14.
+//  Copyright (c) 2014 artemkin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Parsee.
+FOUNDATION_EXPORT double ParseeVersionNumber;
+
+//! Project version string for Parsee.
+FOUNDATION_EXPORT const unsigned char ParseeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parsee/PublicHeader.h>
+
+
